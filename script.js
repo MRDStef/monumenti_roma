977f731ca0b98daccf9817a3fd5949b104a59df8
MRDStef <stefanoandrade9@gmail.com>
2026-04-21 19:33:20 +0200

    Aggiunta mappa Leaflet con centro Roma

---
+++ script.js
@@ -0,0 +1,6 @@
+// Inizializza la mappa centrata su Roma
+var map = L.map('map').setView([41.9028, 12.4964], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
